3bc6d7b3e19abc86e1563ca0ccd8d0232571925a
root <jermaine.mercado7@gmail.com>
2021-06-10 18:00:07 -0400

    Pushing changes to master branch for test deployment.

--- src/api/openWeatherApi.js
+++
@@ -1,6 +0,0 @@
-import apiCall from 'libs/apiCall';
-
-import { OpenWeatherMapKey } from 'config/env';
-
-export const getOpenWeatherForcastApi = (city) =>
-  apiCall(`/forecast?q=${city}&appid=${OpenWeatherMapKey}`);
--- src/config/env.js
+++
@@ -1,1 +0,0 @@
-export const OpenWeatherMapKey = 'acf8af5a4c2c4fb450a39f6a6bc8bdf5';
