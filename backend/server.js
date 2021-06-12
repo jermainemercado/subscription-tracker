3d347aa3fe0b46f55de5707c2f828356e9aadabb
root <jermaine.mercado7@gmail.com>
2021-06-11 21:17:51 -0400

    Fixed imports.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const path = require("path")
-const root = './build'
+const root = '../build'
 const port = process.env.PORT || 5000;
 const session = require('express-session');
 const passport = require('passport');
