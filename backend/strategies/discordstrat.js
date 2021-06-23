4fe13107d3893d14ae81a011ed7deb0c9afdef93
root <jermaine.mercado7@gmail.com>
2021-06-23 00:25:18 -0400

    Changed strategy scope to include bot for Prince join.

--- backend/strategies/discordstrat.js
+++ backend/strategies/discordstrat.js
@@ -51,7 +51,7 @@ passport.use(new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "https://ticketkings-test.herokuapp.com/auth/redirect",
-    scope: ['identify', 'guilds', 'email', 'guilds.join'],
+    scope: ['identify', 'guilds', 'email', 'guilds.join', 'bot'],
     passReqToCallback: true,
 }, async (req, accessToken, refreshToken, profile, done) => {
     try {
