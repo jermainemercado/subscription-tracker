49092eafe9800389842de1b7c0a9647c40921b21
BCarlson1512 <benprskilled@gmail.com>
2021-07-05 22:46:10 -0400

    changed redirect to key page

--- backend/Routers/auth.js
+++ backend/Routers/auth.js
@@ -1,12 +1,14 @@
+/* eslint-disable prettier/prettier */
 require('dotenv').config()
 const router = require('express').Router();
 const passport = require('passport');
 
 router.get('/', passport.authenticate('discord'));
 
+// eslint-disable-next-line prettier/prettier
 router.get('/redirect', passport.authenticate('discord', {
     failiureRedirect: '/forbidden',
-    successRedirect: '/dashboard',
+    successRedirect: '/key',
 }), (req, res) => {
     res.send(req.user);
 })
